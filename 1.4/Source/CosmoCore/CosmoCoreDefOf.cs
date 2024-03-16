e473af216a98ab58bb481ddd3225c2d6314be431
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-16 01:30:19 +0000

    Initial commit

---
+++ 1.4/Source/CosmoCore/CosmoCoreDefOf.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace CosmoCore
+{
+    
+    [DefOf]
+    public static class CosmoCoreDefOf
+    {
+        public static ThingDef CosRim_InsectoidThumper;
+        public static ThingDef CosRim_VRTerminal;
+        public static ThingDef CosRim_VRProgrammingBench;
+        public static ThingDef CosRim_VRChip_Blank;
+
+        public static SoundDef CosRim_Sound_ThumperHit;
+        public static SoundDef CosRim_Sound_ThumperStartup;
+        public static SoundDef CosRim_Sound_ThumperShutdown;
+
+        public static ThingCategoryDef CosRim_VRChips;
+
+        public static ResearchProjectDef CosRim_VRGuideChips;
+    }
+}
---
+++ 1.4/Source/CosmoCore/CosmoCoreStartup.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace CosmoCore
+{
+    [StaticConstructorOnStartup]
+    public static class CosmoCoreStartup
+    {
+        static CosmoCoreStartup()
+        {
+            CosmoCoreSettings s = CosmoCoreMod.settings;
+
+            RunAdjustments_Thumper(s);
+        }
+
+        public static void RunAdjustments_Thumper(CosmoCoreSettings s)
+        {
+            CosmoCoreDefOf.CosRim_InsectoidThumper.specialDisplayRadius = s.thumperRange;
+        }
+    }
+}
